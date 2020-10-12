3c3c7a1327902956dd614d5f4dc217c0f4b51c58
borysjb <66259503+borysjb@users.noreply.github.com>
2020-10-12 23:05:00 +0200

    Yea done
    fuck that, it's done

--- A02/antiPortal.cpp
+++ A02/antiPortal.cpp
@@ -2,72 +2,137 @@
 
 using namespace std;
 
-vector<vector<int>> graph;
-vector <int> enemies;
-queue <int> places;
+vector < vector < int >> graph;
+vector <int> dist;
+queue < int > places;
+bool fuckedup;
 
-bool BFS (int place) {
-  for (int i: graph[place]) {
-    if(enemies[place]!=enemies[i]) {
-      enemies[i]=1-enemies[place];
-      places.push(i);
-    } else {
+void BFS (int place) {
+  for(int i:graph[place]) {
+     if(dist[i] == -1) {
+       dist[i] = dist[place]+1;
+       places.push(i);
+     }
+     if (dist[i]%2==dist[place]%2) {
+       fuckedup=true;
+     }
+  }
+  return;
+}
+
+
+bool funcsLeft () {
+  for(int i=0;i<dist.size();i++) {
+    if(dist[i]==-1){
       return true;
     }
   }
   return false;
 }
 
-int main () {
+int main() {
   int z;
   cin >> z;
   while (z--) {
 
-    int ppl, enem;
-    cin >> ppl >> enem;
-    graph = vector <vector<int>> (ppl);
-    enemies = vector<int> (ppl);
+    int ppl, con;
+    cin >> ppl >> con;
+    graph=vector <vector<int>> (ppl);
+    dist = vector<int> (ppl);
     places = queue <int> ();
+    fuckedup=false;
 
-    for (int i = 0 ; i < enem ; i++) {
-      int temp1, temp2;
-      cin >> temp1 >> temp2;
-      graph [--temp1] .push_back (--temp2);
-      graph [temp2] .push_back (temp1);
+    for (int i = 0; i < con; i++) {
+      int tmp1, tmp2;
+      cin >> tmp1 >> tmp2;
+      graph[--tmp1].push_back(--tmp2);
+      graph[tmp2].push_back(tmp1);
+
     }
 
-    for (int i=0;i<ppl;i++) {
-      enemies[i]=-1;
-    }
-    enemies[0]=0;
+    /*
+    int myNum;
+    cin >> myNum;
+    */
+
     places.push(0);
 
-    /*for (int i=0;i<ppl;i++) {
-      cout<<enemies[i]<<", ";
-    }
-    cout<<endl;*/
 
-    bool FuckUp=true;
+    for (int i = 0; i < ppl; i++) {
+      dist[i] = -1;
+    }
+
+    dist[0] = 0;
 
     while (!places.empty()) {
       int temp = places.front();
       places.pop();
-      bool broken = BFS(temp);
-      if (broken) {
-        cout<<"NIE"<<endl;
-        break;
-        FuckUp=false;
+      BFS(temp);
+      }
+
+
+    /*
+    cout << "Znajomi numeru " << myNum + 1 << ':' << endl;
+
+    for (int y = 0; y < ppl; y++) {
+
+      if (dist[y] > 0) {
+        cout << y + 1 << ": " << dist[y] << endl;
       }
     }
-    if (FuckUp) {
-      for (int i=0;i<ppl;i++) {
-        if (enemies[i]==0)
-          cout<<"A ";
-        else
-          cout<<"B ";
+
+ */
+    //int groups=1;
+    while(funcsLeft()) {
+      for(int i=0;i<ppl;i++) {
+        if(dist[i]==-1) {
+          places.push(i);
+          dist[i]=0;
+          break;
+        }
       }
-      cout<<endl;
+      while (!places.empty()) {
+        int temp = places.front();
+        places.pop();
+        BFS(temp);
+      }
+      //groups++;
     }
+
+  //cout<<"Grup znajomych jest "<<groups<<"."<<endl;
+
+  if(fuckedup) {
+    cout<<"NIE"<<endl;
+  } else {
+    cout<<"TAK"<<endl;
+    for(int i=0;i<ppl;i++) {
+      if(dist[i]%2==0) {
+        cout<<"A ";
+      } else {
+        cout<<"B ";
+      }
+    }
+    cout<<endl;
+  }
+
+  graph.resize(0);
+  dist.resize(0);
   }
   return 0;
 }
+
+/*
+2
+5 6
+1 4
+3 2
+2 1
+4 5
+3 4
+5 2
+4 4
+1 2
+2 3
+2 4
+3 1
+*/
